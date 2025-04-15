4be94e2392355a55c8f04aa794d38207e629ca50
hoangducanh18650 <hoangducanh.1865.0@gmail.com>
2025-04-16 02:39:28 +0700

    Real Estate App Homepage Design + React.js VSCode Snippets

---
+++ src/routes/homePage/homePage.jsx
@@ -0,0 +1,18 @@
+// Use "fcs" for short.
+// Check this GitHub repository for more detail: https://github.com/safak/snippets
+/*
+Do the following steps to be able to use the shortcuts:
+- Step 1: Copy the content in file javascriptreact.json in the repo
+- Step 2: Use Ctrl + Shift + P to open the command palette, type "Snippets" and select "Preferences: Configure User Snippets"
+- Step 3: Select "javascriptreact.json" and paste the content you copied in step 1
+- Step 4: Save the file
+*/
+import './homePage.scss'
+
+function HomePage() {
+    return (
+        <div className='homePage'>HomePage</div>
+    )
+}
+
+export default HomePage
