f240757c7297fa866b3897086d4d147536df1c3c
hoangducanh18650 <hoangducanh.1865.0@gmail.com>
2025-04-16 20:37:48 +0700

    React Router Dom Tutorial 2024 + React Router Dom Outlet Tutorial

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,40 @@
-import "./layout.scss"
-import Navbar from "./components/navbar/Navbar"
 import HomePage from "./routes/homePage/homePage";
+// import * as React from "react";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import ListPage from "./routes/listPage/listPage";
+import Layout from "./routes/layout/layout";
+import SinglePage from "./routes/singlePage/singlePage";
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <HomePage />,
+        },
+        {
+          path: "/list",
+          element: <ListPage />,
+        },
+        {
+          path: "/:id",
+          element: <SinglePage />,
+        }
+      ]
+    },
+  ]);
+
   return (
-    <div className="layout">
-      <div className="navbar">
-        <Navbar />
-      </div>
-      <div className="content">
-        <HomePage />
-      </div>
-    </div>
-  )
+
+    <RouterProvider router={router} />
+  );
 }
 
 export default App;
