37987d5f8d0b17139aed5422bb57b6d9920d123a
hoangducanh18650 <hoangducanh.1865.0@gmail.com>
2025-04-28 04:15:51 +0700

    React.js Image Slider Tutorial From Scratch

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,9 +1,47 @@
+import { useState } from 'react';
 import './slider.scss'
 
-function Slider(){
+function Slider({ images }) {
+  const [imageIndex, setImageIndex] = useState(null);
+
+  const changeSlide = (direction) => {
+    if (direction === 'left') {
+      setImageIndex(imageIndex === 0 ? images.length - 1 : imageIndex - 1);
+    } else {
+      setImageIndex(imageIndex === images.length - 1 ? 0 : imageIndex + 1);
+    }
+  }
+
   return (
-    <div className='slider'>Slider</div>
+    <div className='slider'>
+      {imageIndex != null &&
+        (<div className="fullSlider">
+          <div className="arrow" onClick={() => changeSlide('left')}>
+            <img src="/arrow.png" alt="" />
+          </div>
+
+          <div className="imgContainer">
+            <img src={images[imageIndex]} alt="" />
+          </div>
+
+          <div className="arrow" onClick={() => changeSlide('right')}>
+            <img src="/arrow.png" className='right' alt="" />
+          </div>
+
+          <div className="close" onClick={() => setImageIndex(null)}> X</div>
+        </div>)}
+
+      <div className="bigImage">
+        <img src={images[0]} alt="" onClick={() => setImageIndex(0)} />
+      </div>
+
+      <div className="smallImage">
+        {images.slice(1).map((image, index) => (
+          <img src={image} alt="" key={index} onClick={() => setImageIndex(index + 1)} />
+        ))}
+      </div>
+    </div>
   )
-}  
+}
 
 export default Slider
