29747adc1a8045f5ee71d9f297e50040aad9e43e
hoangducanh18650 <hoangducanh.1865.0@gmail.com>
2025-04-28 16:34:40 +0700

    React Messenger Chat Window Design

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,6 +1,9 @@
+import { useState } from 'react';
 import './chat.scss'
 
 function Chat() {
+    const [chat, setChat] = useState(true);
+
     return (
         <div className='chat'>
             <div className="messages">
@@ -46,7 +49,71 @@ function Chat() {
                     </p>
                 </div>
             </div>
-            <div className="chatBox">box</div>
+            {chat && (<div className="chatBox">
+                <div className="top">
+                    <div className="user">
+                        <img src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+                        Duc Anh
+                    </div>
+                    <span className='close' onClick={() => setChat(null)}>X</span>
+                </div>
+
+                <div className="center">
+                    <div className="chatMessage">
+                        <p>
+                            I want to rent your apartment.
+                        </p>
+                        <span>1 hour ago</span>
+                    </div>
+
+                    <div className="chatMessage own">
+                        <p>
+                            I want to rent your apartment.
+                        </p>
+                        <span>1 hour ago</span>
+                    </div>
+
+                    <div className="chatMessage">
+                        <p>
+                            I want to rent your apartment.
+                        </p>
+                        <span>1 hour ago</span>
+                    </div>
+
+                    <div className="chatMessage own">
+                        <p>
+                            I want to rent your apartment.
+                        </p>
+                        <span>1 hour ago</span>
+                    </div>
+
+                    <div className="chatMessage">
+                        <p>
+                            I want to rent your apartment.
+                        </p>
+                        <span>1 hour ago</span>
+                    </div>
+
+                    <div className="chatMessage own">
+                        <p>
+                            I want to rent your apartment.
+                        </p>
+                        <span>1 hour ago</span>
+                    </div>
+
+                    <div className="chatMessage">
+                        <p>
+                            I want to rent your apartment.
+                        </p>
+                        <span>1 hour ago</span>
+                    </div>
+                </div>
+
+                <div className="bottom">
+                    <textarea name="" id=""></textarea>
+                    <button>Send</button>
+                </div>
+            </div>)}
         </div>
     )
 }
